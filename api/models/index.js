718898336588dd4a8b318c7d1de27bc53a1ff8b4
gness1804 <graham.nessler@gmail.com>
2018-06-09 14:58:47 -0500

    Create basic schema

---
+++ api/models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const PlaceSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Please enter the name of the establishment.',
+  },
+  entryCreationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  address: {
+    type: String,
+    required: 'Please enter a valid address.',
+  },
+  photo: String,
+});
+
+module.exports = model('Place', PlaceSchema);

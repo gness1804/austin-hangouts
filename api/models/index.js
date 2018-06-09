0e7c7dc8fe02986eb0365af34aedd6166cc043cf
gness1804 <graham.nessler@gmail.com>
2018-06-09 15:19:36 -0500

    Set up flow

--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,10 @@
+// @flow
+
 const mongoose = require('mongoose');
 
 const { Schema, model } = mongoose;
 
-const PlaceSchema = new Schema({
+const PlaceSchema: Schema = new Schema({
   name: {
     type: String,
     required: 'Please enter the name of the establishment.',

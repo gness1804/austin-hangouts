3fcedc17f9fd1fa7cae27718acd011e1ad54de20
gness1804 <graham.nessler@gmail.com>
2018-06-09 15:12:32 -0500

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: ['airbnb-base'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'webpack.config.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    'no-console': 'off',
+    'func-names': 'off',
+    'object-shorthand': 'off',
+    'no-alert': 'off',
+    'arrow-body-style': 'off',
+    'global-require': 'off',
+    'flowtype-errors/show-errors': 'off',
+    'no-plusplus': 'off',
+    'max-len': 'off',
+    'no-restricted-syntax': 'off',
+    'no-path-concat': 'off',
+    'no-underscore-dangle': 'off',
+    'import/extensions': ['error', 'always',
+      {
+      'js': 'never',
+      }
+    ],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error',
+      {
+      'optionalDependencies': ['test/unit/index.js'
+        ]
+      }
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}

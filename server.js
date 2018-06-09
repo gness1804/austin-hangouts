45f173db4d2d79e35edd4a140d2a0ed309b66fcd
gness1804 <graham.nessler@gmail.com>
2018-06-09 14:33:06 -0500

    Got server working

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', port);
 
-app.locals.title = ''
+app.locals.title = 'Austin Hangouts'
 
 app.get('/', (request, response) => {
     response.send('');
 });
 
 app.listen(app.get('port'), () => {
-    console.log();
+    console.log(`Server running on port ${port}`);
 });
 

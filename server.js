209fabefaf2ac03ad4743363b0ae8a61d4c8afbb
gness1804 <graham.nessler@gmail.com>
2018-06-09 14:28:44 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000);
+
+app.locals.title = ''
+
+app.get('/', (request, response) => {
+    response.send('');
+});
+
+app.listen(app.get('port'), () => {
+    console.log();
+});
+

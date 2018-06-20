a28e59c384da3598d0a4e85674bad9e3a1042844
gness1804 <graham.nessler@gmail.com>
2018-06-19 19:21:51 -0500

    Add Content-Type header

--- api/controllers/middleware/setHeaders.js
+++ api/controllers/middleware/setHeaders.js
@@ -1,6 +1,7 @@
 const setHeaders = (req, res, next) => {
   res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.set('Access-Control-Allow-Methods', ['GET', 'POST', 'DELETE', 'PUT']);
+  res.set('Access-Control-Allow-Headers', ['Content-Type']);
   return next();
 };
 
